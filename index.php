1fab654771413e764e75f4a496743bbf8537ff6e
Karl Erik Nelson <karlerik.nelson@tptlive.ee>
2024-12-14 09:21:44 +0200

    new

--- index.php
+++ index.php
@@ -4,17 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="<?=get_stylesheet_uri()?>">
+    <!-- <link rel="stylesheet" href="<?=get_stylesheet_uri()?>"> -->
     <?php wp_head() ?>
 </head>
 <body>
-    <?php if(have_posts()): ?>
-        <?php while(have_posts()): ?>
-            <?php the_post() ?>
-            <h1><?=the_title()?></h1>
-            <p><?=the_content()?></p>
-        <?php endwhile ?> 
-    <?php endif ?>
+    <?php get_template_part('partials/nav') ?>
+    <div class="container">
+        <div class="content">
+            <?php if(have_posts()): ?>
+                <?php while(have_posts()): ?>
+                    <?php the_post() ?>
+                    <h1><?=the_title()?></h1>
+                    <p><?=the_content()?></p>
+                <?php endwhile ?> 
+            <?php endif ?>
+        </div>
+    </div>
 <?php wp_footer() ?>
 </body>
 </html>
